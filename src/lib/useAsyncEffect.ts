97d4846466653b85af7ec24f945bd4cf832b96b5
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-21 19:57:26 -0500

    use

--- src/lib/useAsyncEffect.ts
+++ src/lib/useAsyncEffect.ts
@@ -1,48 +1,46 @@
-/**
- * Multiplies a value by 2. (Also a full example of TypeDoc's functionality.)
- *
- * ### Example (es module)
- * ```js
- * import { double } from 'typescript-starter'
- * console.log(double(4))
- * // => 8
- * ```
- *
- * ### Example (commonjs)
- * ```js
- * var double = require('typescript-starter').double;
- * console.log(double(4))
- * // => 8
- * ```
- *
- * @param value - Comment describing the `value` parameter.
- * @returns Comment describing the return type.
- * @anotherNote Some other value.
- */
-export const double = (value: number) => value * 2;
+import { useEffect, useState } from 'react';
 
-/**
- * Raise the value of the first parameter to the power of the second using the
- * es7 exponentiation operator (`**`).
- *
- * ### Example (es module)
- * ```js
- * import { power } from 'typescript-starter'
- * console.log(power(2,3))
- * // => 8
- * ```
- *
- * ### Example (commonjs)
- * ```js
- * var power = require('typescript-starter').power;
- * console.log(power(2,3))
- * // => 8
- * ```
- * @param base - the base to exponentiate
- * @param exponent - the power to which to raise the base
- */
-export const power = (base: number, exponent: number) =>
-  /**
-   * This es7 exponentiation operator is transpiled by TypeScript
-   */
-  base ** exponent;
+export type DependencyList = readonly any[];
+export type AsyncEffect<T> = (mounted: boolean) => Promise<T> | T;
+export type CleanupFunc<T> = (result: T | undefined) => void;
+
+export function useAsyncEffect<T>(
+  effect: AsyncEffect<T>,
+  dependencies?: DependencyList
+): void;
+export function useAsyncEffect<T>(
+  effect: AsyncEffect<T>,
+  cleanup?: CleanupFunc<T>,
+  dependencies?: DependencyList
+): void;
+export function useAsyncEffect<T>(
+  effect: AsyncEffect<T>,
+  cleanup?: CleanupFunc<T> | DependencyList,
+  dependencies?: DependencyList
+): void {
+  // Has cleanup function
+  const hasCleanup = typeof cleanup === 'function';
+  const deps = hasCleanup
+    ? dependencies?.concat(cleanup) || [cleanup]
+    : ((cleanup as unknown) as DependencyList) || [];
+  const [mounted, setMounted] = useState(false);
+  const [result, setResult] = useState<T>();
+  useEffect(() => {
+    setMounted(true);
+
+    Promise.resolve(effect(mounted))
+      .then(setResult)
+      .catch((e) => {
+        throw e;
+      });
+
+    return () => {
+      setMounted(false);
+
+      if (hasCleanup) {
+        ((cleanup as unknown) as CleanupFunc<T>)(result);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+}

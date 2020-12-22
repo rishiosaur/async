56ec77bb5ca6fddfb6e246c19e8a65afea4443a0
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-22 10:01:26 -0500

    documentation

--- src/lib/useAsyncEffect.ts
+++ src/lib/useAsyncEffect.ts
@@ -1,13 +1,43 @@
 import { useEffect, useState } from 'react';
 
+/**
+ * Dependency list for effects.
+ */
 export type DependencyList = readonly any[];
+
+/**
+ * An asynchronous effect that may return something.
+ * @template T Type that the effect returns. Note: in a lot of cases, when used with [[useAsyncEffect]], this can be `void`.
+ */
 export type AsyncEffect<T> = (mounted: boolean) => Promise<T>;
+
+/**
+ * Cleanup function used on destruction of component (on unmount).
+ * @template T Type that the original effect returns (may be undefined, in the case that T is `void`).
+ */
 export type CleanupFunc<T> = (result: T | undefined) => void;
 
+/**
+ * Base Asynchronous Effect Hook.
+ *
+ * @export
+ * @template T Result of your effect.
+ * @param {AsyncEffect<T>} effect An effect function that returns `Promise<T>`. Accepts argument of `mounted`.
+ * @param {DependencyList} [dependencies] Dependency array for hooks (taken from React)
+ */
 export function useAsyncEffect<T>(
   effect: AsyncEffect<T>,
   dependencies?: DependencyList
 ): void;
+/**
+ * Asynchronous Effect Hook with support for a cleanup callback.
+ *
+ * @export
+ * @template T Result of your effect.
+ * @param {AsyncEffect<T>} effect An effect function that returns `Promise<T>`. Accepts argument of `mounted`
+ * @param {CleanupFunc<T>} [cleanup] A cleanup effect run after the component dismounts. Takes in result of effect (if any)
+ * @param {DependencyList} [dependencies] Dependency array for hooks (taken from React)
+ */
 export function useAsyncEffect<T>(
   effect: AsyncEffect<T>,
   cleanup?: CleanupFunc<T>,
--- src/lib/usePromiseEffect.ts
+++ src/lib/usePromiseEffect.ts
@@ -1,15 +1,37 @@
 import { useState } from 'react';
 import { useAsyncEffect, AsyncEffect, DependencyList } from './useAsyncEffect';
 
-type PromiseEffectState<T> =
+/**
+ * Possible states of the Promise effect used in [[usePromiseEffect]].
+ * @template T Return type of the effect response.
+ */
+export type PromiseEffectState<T> =
   | { status: 'pending'; value: null; error: null }
   | { status: 'fulfilled'; value: T; error: null }
   | { status: 'rejected'; value: null; error: Error };
 
+/**
+ * A Promise effect that builds on [[useAsyncEffect]] with state management built in.
+ *
+ * @export
+ * @template T Result of [[effect]]
+ * @param {AsyncEffect<T>} effect The Asynchronous effect that returns `Promise<T>`
+ * @param {DependencyList} [deps] Dependency array.
+ * @returns {((boolean | Error | T | null)[])} States of the promise at any given time.
+ * @example
+ * ```typescript
+ *  // In a component
+ *  const [value, loading, err] = usePromiseEffect(() =>
+      fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) =>
+        response.json()
+      ) // Catching is automatically supported, and can be accessed through `err`.
+    );
+ * ```
+ */
 export function usePromiseEffect<T>(
   effect: AsyncEffect<T>,
   deps?: DependencyList
-): (boolean | Error | T | null)[] {
+): [T | null, boolean, Error | null] {
   const [{ status, value, error }, setPromiseState] = useState<
     PromiseEffectState<T>
   >({
